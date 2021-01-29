9f16ee83fd9e62d106159f410a7f642c9d97de52
赵明明 <13472293379@163.com>
2021-01-29 18:39:51 +0800

    图标

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.al);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                btn.setText("我被点击了");
+            }
+        });
     }
 }
